972824eb5fd5bef58a2f9b170510f5c25e712e59
Tehnikumi Opilane <opilane@tptlive.ee>
2019-03-13 11:29:51 +0200

    Random stuff

--- components/Upgrade.vue
+++ components/Upgrade.vue
@@ -1,31 +1,58 @@
 <template>
+  <div>
     <button @click="addToPerSecond" class="upgrade v-btn" :disabled="!canBuy" :class="{ disabled: !canBuy }">
       <h3>{{label}}</h3>
-      <h3>Adds {{addAmount}} per second</h3>
+      <h3>Adds {{realAddAmount}} per second</h3>
       <h3>Costs {{Math.round(realCost)}}</h3>
     </button>
+    <div v-show="upgradeAmount != 0" class="upgradeAmount">
+      <div v-for="index in upgradeAmount" :key="index" class="upgradeIndicator" :class="{ green: upgradedEfficiency }"></div>
+    </div>
+    <button
+      v-show="!upgradedEfficiency"
+      @click="upgradeEfficiency"
+      :disabled="!canBuyEfficiency"
+      :class="{ disabled: !canBuyEfficiency }"
+      class="upgrade v-btn green">
+      <h3>Upgrade Efficiency, Costs {{efficiencyCost}}</h3>
+    </button>
+  </div>
 </template>
 
 <script>
 export default {
-  props: ["addAmount", "cost", "label"],
+  props: ["addAmount", "cost", "label", "efficiencyCost"],
   name: "Upgrade",
   data() {
     return {
-      realCost: +this.cost
+      realCost: +this.cost,
+      realAddAmount: this.addAmount,
+      upgradeAmount: 0,
+      upgradedEfficiency: false
     }
   },
   methods: {
     addToPerSecond() {
       this.$store.dispatch('addToCookies', -this.realCost);
-      console.log(Math.sqrt(this.realCost));
+      this.upgradeAmount++;
       this.realCost += +Math.sqrt(this.realCost);
-      this.$store.dispatch('addToPerSecond', this.addAmount);
+      this.$store.dispatch('addToPerSecond', this.realAddAmount);
+    },
+    upgradeEfficiency() {
+      this.$store.dispatch('addToCookies', -this.efficiencyCost);
+      this.upgradedEfficiency = true;
+      const addPerUpgrade = this.realAddAmount * 2;
+      // Math.round is needed for correct summation
+      this.realAddAmount = Math.round((+this.realAddAmount + addPerUpgrade) * 10) / 10;
+      this.$store.dispatch('addToPerSecond', addPerUpgrade * this.upgradeAmount);
     }
   },
   computed: {
     canBuy() {
       return this.$store.state.cookieAmount >= this.realCost;
+    },
+    canBuyEfficiency() {
+      return this.$store.state.cookieAmount >= this.efficiencyCost;
     }
   }
 }
@@ -37,8 +64,19 @@ export default {
   color: white;
   background-color: #ba2323;
 }
+.upgradeAmount {
+  width: 80%;
+  display:inline-block;
+}
+.upgradeIndicator {
+  margin: 1px 2px;
+  background-color: white;
+  height: 14px;
+  width: 5px;
+  float: left;
+}
 .upgrade:hover {
-  width: 85%;
+  width: 81%;
 }
 .disabled {
   color: black;
--- pages/index.vue
+++ pages/index.vue
@@ -4,35 +4,38 @@
     <br>
     <button @click="incrementCookie" class="cookie" :class=""><img src="../static/cookie.png"></button>
     <br>
-    <h2>{{Math.round(cookieAmount)}}</h2>
+    <h2>{{beautifiedCookieAmount}}</h2>
     <h3>Per second: {{+perSecond.toFixed(2)}}</h3>
     <br>
-    <Upgrade cost="15" add-amount="0.2" label="Upgrade 1"></Upgrade>
-    <Upgrade cost="100" add-amount="2" label="Upgrade 2"></Upgrade>
-    <Upgrade cost="1000" add-amount="20" label="Upgrade 3"></Upgrade>
-    <Upgrade cost="10000" add-amount="100" label="Upgrade 4"></Upgrade>
-    <Upgrade cost="100000" add-amount="600" label="Upgrade 5"></Upgrade>
-    <Upgrade cost="1000000" add-amount="2000" label="Upgrade 6"></Upgrade>
+    <Upgrade cost="15" add-amount="0.2" label="Upgrade 1" efficiency-cost="3000"></Upgrade>
+    <Upgrade cost="100" add-amount="2" label="Upgrade 2" efficiency-cost="10000"></Upgrade>
+    <Upgrade cost="1000" add-amount="20" label="Upgrade 3" efficiency-cost="40000"></Upgrade>
+    <Upgrade cost="10000" add-amount="100" label="Upgrade 4" efficiency-cost="100000"></Upgrade>
+    <Upgrade cost="100000" add-amount="600" label="Upgrade 5" efficiency-cost="500000"></Upgrade>
+    <Upgrade cost="1000000" add-amount="5000" label="Upgrade 6" efficiency-cost="10000000"></Upgrade>
   </div>
 </template>
 <script>
 import Upgrade from "../components/Upgrade";
+import numeral from 'numeral';
 export default {
   components: { Upgrade },
   created() {
     this.setTitle();
     const store = this.$store;
     setInterval(() => {
-      this.setTitle();
       store.dispatch('addToCookies');
     }, 10);
+    setInterval(() => {
+      this.setTitle();
+    }, 1000)
   },
   methods: {
     incrementCookie() {
       this.$store.dispatch('incrementCookie');
     },
     setTitle() {
-      document.title = Math.round(this.cookieAmount) + " cookies";
+      document.title = this.beautifiedCookieAmount + " cookies";
     }
   },
   computed: {
@@ -41,6 +44,11 @@ export default {
     },
     cookieAmount() {
       return this.$store.state.cookieAmount;
+    },
+    beautifiedCookieAmount() {
+      const roundedAmount = Math.round(this.$store.state.cookieAmount);
+      if (roundedAmount < 1000) return roundedAmount;
+      return numeral(roundedAmount).format('0.00a');
     }
   }
 }
